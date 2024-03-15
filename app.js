61330769d08fb0a0dd41972f059627a9a91e7e90
tee1ma <teemu.weck@gmail.com>
2024-03-15 09:13:20 +0200

    implemented automatic server closing

--- app.js
+++ app.js
@@ -12,48 +12,49 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
-    console.log(`New ${req.method} request to ${req.path} by ${req.ip}`);
-    next();
+  console.log(`New ${req.method} request to ${req.path} by ${req.ip}`);
+  console.log(games);
+  next();
 });
 
 app.get("/home", (req, res) => {
-    res.render("home", { games });
+  res.render("home", { games });
 });
 
 app.post("/create", (req, res) => {
-    const id = req.body.id.replaceAll(/\s/g, "").replaceAll(" ", "");
-    if (games.some((game) => game.id === id || games.length > 20)) {
-        res.redirect("/home");
-    } else {
-        const game = new GAME(id);
-        games.push(game);
-        res.redirect("/game/" + game.id);
-    }
+  const id = req.body.id.replaceAll(/\s/g, "").replaceAll(" ", "");
+  if (games.some((game) => game.id === id || games.length > 20)) {
+    res.redirect("/home");
+  } else {
+    const game = new GAME(id, games);
+    games.push(game);
+    res.redirect("/game/" + game.id);
+  }
 });
 
 app.get("/game/:id", (req, res) => {
-    const id = req.params.id;
-    const game = games.find((game) => game.id === id);
-    if (game && !game.IsFull() && !game.hasStarted) {
-        res.render("game", game);
-    } else {
-        res.redirect("/home");
-    }
+  const id = req.params.id;
+  const game = games.find((game) => game.id === id);
+  if (game && !game.IsFull() && !game.hasStarted) {
+    res.render("game", game);
+  } else {
+    res.redirect("/home");
+  }
 });
 
 app.use((req, res) => {
-    res.redirect("/home");
+  res.redirect("/home");
 });
 
 server.on("upgrade", (req, socket, head) => {
-    const id = req.url.slice(1);
-    const game = games.find((game) => game.id === id);
-    game.wss.handleUpgrade(req, socket, head, (ws) => {
-        game.wss.emit("connection", ws, socket);
-    });
+  const id = req.url.slice(1);
+  const game = games.find((game) => game.id === id);
+  game.wss.handleUpgrade(req, socket, head, (ws) => {
+    game.wss.emit("connection", ws, socket);
+  });
 })
 
 const port = 80;
 server.listen(port, () => {
-    console.log("Server running on port", port)
+  console.log("Server running on port", port)
 });
--- game.js
+++ game.js
@@ -1,228 +1,228 @@
 const WebSocket = require("ws");
 
 class GAME {
-    constructor(id) {
-        this.id = id;
-        this.maxPlayers = 20;
-        this.startingStack = 1000;
-        this.hasStarted = false;
-        this.timer = 30;
-        this.players = [];
-        this.wss = new WebSocket.WebSocketServer({ noServer: true });
-        this.HandleWSS(this.wss);
+  constructor(id, games) {
+    this.id = id;
+    this.maxPlayers = 20;
+    this.startingStack = 1000;
+    this.hasStarted = false;
+    this.timer = 30;
+    this.players = [];
+    this.wss = new WebSocket.WebSocketServer({ noServer: true });
+    this.HandleWSS(this.wss, games);
+  }
+  IsFull() { return this.players.length >= this.maxPlayers; }
+
+  CreatePrizes(amount, totalPoints) {
+    const remainder = 1000 % amount;
+    const base = (1000 - remainder) / amount;
+    const step = Math.floor(40 / amount);
+    let prizes = [];
+    for (let i = 0; i <= amount; i++) {
+      if (i === amount / 2) { i++; }
+      const permil = base + (step * (i - (amount / 2)));
+      const prize = totalPoints * permil / 1000;
+      prizes.push(prize);
     }
-    IsFull() { return this.players.length >= this.maxPlayers; }
+    prizes[0]+= remainder;
+    return prizes;
+  }
 
-    CreatePrizes(amount, totalPoints) {
-        const remainder = 1000 % amount;
-        const base = (1000 - remainder) / amount;
-        const step = Math.floor(40 / amount);
-        let prizes = [];
-        for (let i = 0; i <= amount; i++) {
-            if (i === amount / 2) { i++; }
-            const permil = base + (step * (i - (amount / 2)));
-            const prize = totalPoints * permil / 1000;
-            prizes.push(prize);
-        }
-        prizes[0]+= remainder;
-        return prizes;
+  SelectWinners(prize) {
+    const players = this.players.filter((player) => !player.busted);
+    const max = players.reduce((a, b) => {
+      return a.pointsBid > b.pointsBid ? a : b;
+    }).pointsBid;
+    const winners = players.filter(player => player.pointsBid === max);
+    let winnernames = [];
+    winners.forEach((winner) => {
+      winner.pointsWon.push(Math.floor(prize / winners.length));
+      winnernames.push(winner.nickname);
+    });
+    return winnernames;
+  }
+
+  StartGame() {
+    this.hasStarted = true;
+    this.players.forEach((player) => {
+      player.busted = false;
+      player.pointsWon = [this.startingStack];
+      player.startingPoints = 0;
+      player.pointsBid = 0;
+    });
+    this.SendToClients(["STARTGAME", this.timer]);
+
+    setTimeout(() => {
+      //A
+      this.players.forEach((player) => { player.NewRoundReset(); });
+      this.UpdatePlayerList();
+      const totalPoints = this.players.length * this.startingStack;
+      const prizes = this.CreatePrizes(this.players.length * 2, totalPoints);
+      this.SendToClients(["PRIZES", prizes]);
+      this.players.forEach((player) => { player.pointsBid = 0; });
+      this.SendToClients(["STARTBIDDING", 0]);
+
+      setTimeout(() => {
+        this.MainLoop(0, prizes, totalPoints);
+      }, this.timer * 1000)
+    }, 3000);
+
+  }
+
+  MainLoop(bettingRound, prizes, totalPoints) {
+
+    //B
+    const winners = this.SelectWinners(prizes[bettingRound]);
+    this.SendToClients(["ROUNDWINNER", [winners, Math.floor(prizes[bettingRound] / winners.length), this.players.find(player => player.nickname === winners[0]).pointsBid]]);
+    this.UpdatePlayerList();
+
+    this.players.forEach((player) => { player.pointsLeft -= player.pointsBid; });
+
+    if (bettingRound === prizes.length - 1) { bettingRound = 0; }
+    else { bettingRound++; }
+
+    if (bettingRound === 0) {
+      this.players.forEach((player) => { player.NewRoundReset(); });
+      this.UpdatePlayerList();
+      if (prizes.length > 2) {
+        prizes = this.CreatePrizes(prizes.length - 2, totalPoints);
+        this.SendToClients(["PRIZES", prizes]);
+      } else {
+        prizes = [];
+      }
     }
 
-    SelectWinners(prize) {
-        const players = this.players.filter((player) => !player.busted);
-        const max = players.reduce((a, b) => {
-            return a.pointsBid > b.pointsBid ? a : b;
-        }).pointsBid;
-        const winners = players.filter(player => player.pointsBid === max);
-        let winnernames = [];
-        winners.forEach((winner) => {
-            winner.pointsWon.push(Math.floor(prize / winners.length));
-            winnernames.push(winner.nickname);
-        });
-        return winnernames;
+    if (this.players.filter(player => !player.busted).length > 1 && prizes.length > 0) {
+      //A
+      this.players.forEach((player) => { player.pointsBid = 0; });
+      this.SendToClients(["STARTBIDDING", bettingRound]);
+
+      setTimeout(() => {
+        this.MainLoop(bettingRound, prizes, totalPoints); 
+      }, this.timer * 1000);
+    } else {
+      this.hasStarted = false;
+
+      const players = this.players.filter((player) => !player.busted);
+      const winner = players.reduce((a, b) => {
+        return a.startingPoints > b.startingPoints ? a : b;
+      });
+      this.SendToClients(["GAMEWINNER", winner.nickname]);
     }
+  }
 
-    StartGame() {
-        this.hasStarted = true;
-        this.players.forEach((player) => {
-            player.busted = false;
-            player.pointsWon = [this.startingStack];
-            player.startingPoints = 0;
-            player.pointsBid = 0;
-        });
-        this.SendToClients(["STARTGAME", this.timer]);
-
-        setTimeout(() => {
-            //A
-            this.players.forEach((player) => { player.NewRoundReset(); });
-            this.UpdatePlayerList();
-            const totalPoints = this.players.length * this.startingStack;
-            const prizes = this.CreatePrizes(this.players.length * 2, totalPoints);
-            this.SendToClients(["PRIZES", prizes]);
-            this.players.forEach((player) => { player.pointsBid = 0; });
-            this.SendToClients(["STARTBIDDING", 0]);
-
-            setTimeout(() => {
-                this.MainLoop(0, prizes, totalPoints);
-            }, this.timer * 1000)
-        }, 3000);
-
-    }
-
-    MainLoop(bettingRound, prizes, totalPoints) {
-
-        //B
-        const winners = this.SelectWinners(prizes[bettingRound]);
-        this.SendToClients(["ROUNDWINNER", [winners, Math.floor(prizes[bettingRound] / winners.length), this.players.find(player => player.nickname === winners[0]).pointsBid]]);
+  HandleWSS(wss, games) {
+    wss.on("connection", (ws, socket) => {
+      if (this.IsFull() || this.hasStarted) {
+        console.log("Connection denied. Server is full!");
+        socket.destroy();
+      } else {
+        console.log("Player connected");
+        const newPlayer = new PLAYER(ws);
+        this.players.push(newPlayer);
+        if (this.players.length === 1) { newPlayer.admin = true; }
         this.UpdatePlayerList();
 
-        this.players.forEach((player) => { player.pointsLeft -= player.pointsBid; });
-
-        if (bettingRound === prizes.length - 1) { bettingRound = 0; }
-        else { bettingRound++; }
-
-        if (bettingRound === 0) {
-            this.players.forEach((player) => { player.NewRoundReset(); });
-            this.UpdatePlayerList();
-            if (prizes.length > 2) {
-                prizes = this.CreatePrizes(prizes.length - 2, totalPoints);
-                this.SendToClients(["PRIZES", prizes]);
-            } else {
-                prizes = [];
-            }
-        }
-
-        if (this.players.filter(player => !player.busted).length > 1 && prizes.length > 0) {
-            //A
-            this.players.forEach((player) => { player.pointsBid = 0; });
-            this.SendToClients(["STARTBIDDING", bettingRound]);
-
-            setTimeout(() => {
-                this.MainLoop(bettingRound, prizes, totalPoints); 
-            }, this.timer * 1000);
-        } else {
-            this.hasStarted = false;
-
-            const players = this.players.filter((player) => !player.busted);
-            const winner = players.reduce((a, b) => {
-                return a.startingPoints > b.startingPoints ? a : b;
+        ws.on("close", () => {
+          console.log("Player left the server");
+          this.players.splice(this.players.findIndex((player) => player === newPlayer), 1);
+          if (this.players.length === 0) {
+            wss.close(() => {
+              const index = games.findIndex(game => game.id === this.id);
+              games.splice(index, 1);
             });
-            this.SendToClients(["GAMEWINNER", winner.nickname]);
+          } else if (newPlayer.admin) {
+            this.players[0].admin = true;
+          }
+          this.UpdatePlayerList();
+        });
+
+        ws.on("message", (message) => {
+          console.log(`(${this.id}) recieved: ${message}`);
+          const wsmessage = JSON.parse(message);
+          this.OnWsMessage(wsmessage[0], wsmessage[1], ws);
+        });
+
+      }
+    })
+  }
+  SendToClients(data) {
+    console.log(`(${this.id}) sent ${JSON.stringify(data)}`);
+    this.players.forEach((player) => {
+      player.ws.send(JSON.stringify(data));
+    })
+  }
+  UpdatePlayerList() {
+    const playerlist = [];
+    this.players.forEach((player) => {
+      playerlist.push(player.PublicInfo());
+    })
+    this.SendToClients(["PLAYERLIST", playerlist]);
+  }
+
+  OnWsMessage(type, data, ws) {
+    switch (type) {
+      case "NICKNAME":
+        if (data == null || data.replaceAll(/\s/g, "").replaceAll(" ", "").length < 2 || this.players.find((player) => player.nickname === data)) {
+          data = "guest_" + (1000 + Math.floor(Math.random() * 8999)).toString();
+          ws.send(JSON.stringify(["YOURNAME", data]));
         }
-    }
+        this.players.find((player) => player.ws === ws).nickname = data;
+        this.UpdatePlayerList();
+        break;
 
-    HandleWSS(wss) {
-        wss.on("connection", (ws, socket) => {
-            if (this.IsFull() || this.hasStarted) {
-                console.log("Connection denied. Server is full!");
-                socket.destroy();
-            } else {
-                console.log("Player connected");
-                const newPlayer = new PLAYER(ws);
-                this.players.push(newPlayer);
-                if (this.players.length === 1) { newPlayer.admin = true; }
-                this.UpdatePlayerList();
-
-                ws.on("close", () => {
-                    console.log("Player left the server");
-                    this.players.splice(this.players.findIndex((player) => player === newPlayer), 1);
-                    if (this.players.length === 0) {
-                        //Close the server here
-                    } else if (newPlayer.admin) {
-                        this.players[0].admin = true;
-                    }
-                    this.UpdatePlayerList();
-                });
-
-                ws.on("message", (message) => {
-                    console.log(`(${this.id}) recieved: ${message}`);
-                    const wsmessage = JSON.parse(message);
-                    this.OnWsMessage(wsmessage[0], wsmessage[1], ws);
-                });
-
-            }
-        })
-    }
-    CloseWSS(wss){
-
-    }
-    SendToClients(data) {
-        console.log(`(${this.id}) sent ${JSON.stringify(data)}`);
-        this.players.forEach((player) => {
-            player.ws.send(JSON.stringify(data));
-        })
-    }
-    UpdatePlayerList() {
-        const playerlist = [];
-        this.players.forEach((player) => {
-            playerlist.push(player.PublicInfo());
-        })
-        this.SendToClients(["PLAYERLIST", playerlist]);
-    }
-
-    OnWsMessage(type, data, ws) {
-        switch (type) {
-            case "NICKNAME":
-                if (data == null || data.replaceAll(/\s/g, "").replaceAll(" ", "").length < 2 || this.players.find((player) => player.nickname === data)) {
-                    data = "guest_" + (1000 + Math.floor(Math.random() * 8999)).toString();
-                    ws.send(JSON.stringify(["YOURNAME", data]));
-                }
-                this.players.find((player) => player.ws === ws).nickname = data;
-                this.UpdatePlayerList();
-                break;
-
-            case "STARTGAME":
-                if (this.players.length > 1) {
-                    this.StartGame();
-                }
-                break;
-
-            case "BID":
-                const bidder = this.players.find((player) => player.ws === ws);
-                bidder.Bid(data);
-                break;
+      case "STARTGAME":
+        if (this.players.length > 1) {
+          this.StartGame();
         }
-    }
+        break;
 
-    
+      case "BID":
+        const bidder = this.players.find((player) => player.ws === ws);
+        bidder.Bid(data);
+        break;
+    }
+  }
+
+
 }
 class PLAYER {
-    constructor(ws){
-        this.nickname = "guest";
-        this.ws = ws;
-        this.pointsLeft;
-        this.pointsBid;
-        this.startingPoints = 0;
-        this.pointsWon = [];
-        this.admin = false;
-        this.busted = false;
+  constructor(ws){
+    this.nickname = "guest";
+    this.ws = ws;
+    this.pointsLeft;
+    this.pointsBid;
+    this.startingPoints = 0;
+    this.pointsWon = [];
+    this.admin = false;
+    this.busted = false;
+  }
+  PublicInfo() {
+    const playerinfo = {
+      nickname: this.nickname,
+      startingPoints: this.startingPoints,
+      pointsWon: this.pointsWon,
+      admin: this.admin,
+      busted: this.busted
     }
-    PublicInfo() {
-        const playerinfo = {
-            nickname: this.nickname,
-            startingPoints: this.startingPoints,
-            pointsWon: this.pointsWon,
-            admin: this.admin,
-            busted: this.busted
-        }
-        return playerinfo;
+    return playerinfo;
+  }
+  NewRoundReset() {
+    if (this.pointsWon.length === 0) { this.busted = true; }
+    if (this.busted) { this.startingPoints = 0; }
+    else {
+      this.startingPoints = this.pointsWon.reduce((a, b) => { return a+b; }, 0);
+      this.pointsLeft = this.startingPoints;
+      this.pointsWon = [];
     }
-    NewRoundReset() {
-        if (this.pointsWon.length === 0) { this.busted = true; }
-        if (this.busted) { this.startingPoints = 0; }
-        else {
-            this.startingPoints = this.pointsWon.reduce((a, b) => { return a+b; }, 0);
-            this.pointsLeft = this.startingPoints;
-            this.pointsWon = [];
-        }
+  }
+  Bid(amount) {
+    if (this.pointsLeft >= amount && amount > 0) {
+      this.pointsBid = amount;
+    } else {
+      this.pointsBid = 0;
     }
-    Bid(amount) {
-        if (this.pointsLeft >= amount && amount > 0) {
-            this.pointsBid = amount;
-        } else {
-            this.pointsBid = 0;
-        }
-    }
+  }
 }
 
 module.exports = GAME;
